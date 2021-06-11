3f77e720d8f4b08ef80ba7438f312c5ebcdc414c
yuxuanmian <1214386527@qq.com>
2021-06-11 18:21:49 +0800

    余轩冕

--- src/main/java/com/servlet/login_verification.java
+++ src/main/java/com/servlet/login_verification.java
@@ -1,6 +1,7 @@
 package com.servlet;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.beans.LoginInfoBean;
 import com.controller.LoginInfoCon;
 
@@ -10,7 +11,9 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "Servlet", value = "/Servlet")
+@WebServlet(name = "Servlet", urlPatterns = "/verification")
+
+
 public class login_verification extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,8 +23,11 @@ public class login_verification extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String username = request.getParameter("username").trim();
-        String password = request.getParameter("password").trim();
+
+        JSONObject obj=JSON.parseObject(request.getParameter("data"));
+        String username=obj.getString("username");
+        String password=obj.getString("password");
+
         LoginInfoBean loginInfoBean = new LoginInfoBean();
         loginInfoBean.setPassword(password);
         loginInfoBean.setUsername(username);
@@ -33,17 +39,14 @@ public class login_verification extends HttpServlet {
         if (flag) {
             String jsonString = "{state:'success'}";
             out.println(jsonString);
-        }
-
-        else{
+            HttpSession session = request.getSession();
+            session.setAttribute("user", loginInfoBean);
+        } else {
             String jsonString = "{state:'fail'}";
             out.println(jsonString);
         }
-        HttpSession session = request.getSession();
-        session.setAttribute("user", loginInfoBean);
-        ServletContext application = this.getServletContext();
-        RequestDispatcher rd = application.getRequestDispatcher("login.html");
-        rd.forward(request, response);
+
+
     }
 }
 
